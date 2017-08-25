1bc4b2b8c6760c3a5646d13f23a758d85226c506
Rebeca Medeiros <rebeca.andrade.medeiros@gmail.com>
2017-08-25 11:44:49 -0300

    Update Data.cpp

--- Data.cpp
+++ Data.cpp
@@ -13,29 +13,37 @@ Data::Data(int dia, int mes, int ano){
 
     if(dia == 28 && mes != 2){
         std::cout << "Numero de dias invalido para o mes" << std::endl;
+	std::cout << "1" << std::endl;
     }
 
-    if((mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12) && (dia != 31)){
+    if((mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12) && (dia > 31)){
         std::cout << "Numero de dias invalido para o mes" << std::endl;
+	std::cout << "2" << std::endl;
     }
 
-    if((mes == 4 || mes == 6 || mes == 9 || mes == 11) && (dia != 30)){
+    if((mes == 4 || mes == 6 || mes == 9 || mes == 11) && (dia > 30)){
         std::cout << "Numero de dias invalido para o mes" << std::endl;
+	std::cout << "3" << std::endl;
     }
 }
 
 
-void Data::avancarDia(){
-    dia++;
+void Data::avancarDia(int dia, int mes, int ano){
 
     if(dia == 31){
         dia = 1;
         mes++;
-	std::cout << dia << "/" << 
+	std::cout << "Nova data: " << dia << "/" << mes << "/" << ano << std::endl;
+    } else {
+	dia++;
+    	std::cout << "Nova data: " << dia << "/" << mes << "/" << ano << std::endl;
+    }
+    
+    if(mes == 12 && dia == 31){
+        ano++;
+	mes = 1;
+	std::cout << "Nova data: " << dia << "/" << mes << "/" << ano << std::endl;
     }
 
-    if(mes == 12 && dia == 31){
-        mes = 1;
-        ano++;
-    }
+   
 }
