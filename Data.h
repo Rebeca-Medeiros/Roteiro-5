b2fbb02c20d8a5144c2a9dbffac9aa1f0a7ac704
Rebeca Medeiros <rebeca.andrade.medeiros@gmail.com>
2017-09-05 10:17:45 -0300

    Add files via upload

--- Data.h
+++ Data.h
@@ -2,13 +2,23 @@
 #define DATA_H
 
 class Data {
-    public:
+    private:
         int dia;
         int mes;
         int ano;
-
+    public:
         Data(int dia, int mes, int ano);
         void avancarDia(int dia, int mes, int ano);
+	
+	int getDia();
+	void setDia(int dia);
+	
+	int getMes();
+	void setMes(int mes);
+	
+	int getAno();
+	void setAno(int ano);
+	
 };
 
 #endif // DATA_H
