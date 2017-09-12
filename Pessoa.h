d6b10534a8c7ae720a82f22c9be48d1178e60dba
Rebeca Medeiros <rebeca.andrade.medeiros@gmail.com>
2017-09-11 23:53:55 -0300

    Add files via upload

---
+++ Pessoa.h
@@ -0,0 +1,25 @@
+#ifndef PESSOA_H
+#define PESSOA_H
+
+#include <iostream>
+
+class Pessoa{
+  private:
+    std::string nome;
+    int idade;
+    std::string telefone;
+  public:
+    Pessoa(std::string nome);
+    Pessoa(std::string nome, int idade, std::string telefone);
+
+    std::string getNome();
+    void setNome(std::string n);
+
+    int getIdade();
+    void setIdade(int i);
+
+    std::string getTelefone();
+    void setTelefone(std::string t);
+};
+
+#endif // PESSOA_H
