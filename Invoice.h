bd25372fc83fbea0245e7a52938893408a58218b
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2017-09-12 11:16:30 -0300

    Add files via upload

--- Invoice.h
+++ Invoice.h
@@ -3,14 +3,28 @@
 #include <iostream>
 
 class Invoice{
-	public: 
+	private: 
 		int numero;
 		std::string descricao;
 		int quantidade;
 		double precop;
-	
+	public:
 		Invoice(int numero, std::string descricao, int quantidade, double precop);
 		double getInvoiceAmount(int quantidade, double preco);
+
+		int getNumero();
+		void setNumero(int n);
+	
+		int getQuantidade();
+		void setQuantidade(int q);
+	
+		std::string getDescricao();
+		void setDescricao(std::string s);
+
+		double getPrecop();
+		void setPrecop(double p);
+
+		
 };
 
 
