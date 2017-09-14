3d07f735ec204895a0b9c6d8b23221805efb0185
Rebeca Medeiros <rebeca.andrade.medeiros@gmail.com>
2017-09-14 10:56:34 -0300

    Update mainEmpregadoTest.cpp

--- mainEmpregadoTest.cpp
+++ mainEmpregadoTest.cpp
@@ -8,8 +8,8 @@ int main(){
 	Empregado e1 = Empregado("Rosa", "Diaz", 1250);
 	Empregado e2 = Empregado("Amelia", "Santiago", 1350);
 
-	cout << e1.getNome << " " << e1.getSobrenome << " Salario mensal: " << e1.getSalarioM << endl;
-	cout << e2.getNome << " " << e1.getSobrenome << " Salario mensal: " << e2.getSalarioM << endl;
+	cout << e1.getNome() << " " << e1.getSobrenome() << " Salario mensal: " << e1.getSalarioM() << endl;
+	cout << e2.getNome() << " " << e1.getSobrenome() << " Salario mensal: " << e2.getSalarioM() << endl;
 
 	e1.salarioAnual(e1.getSalarioM());
 	e2.salarioAnual(e2.getSalarioM());
