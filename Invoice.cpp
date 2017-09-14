7215f07e7c5790bf9264cff8c2d88a124ed7dba6
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2017-09-14 11:20:57 -0300

    Add files via upload

--- Invoice.cpp
+++ Invoice.cpp
@@ -1,4 +1,5 @@
 #include "Invoice.h"
+#include <string>
 #include <iostream>
 
 void Invoice::setNumero(int n){
@@ -10,7 +11,7 @@ void Invoice::setQuantidade(int q){
 }
 
 void Invoice::setDescricao(std::string s){
-	std::string descricao = std::string s;
+	descricao = s;
 }
 
 void Invoice::setPrecop(double p){
@@ -25,37 +26,39 @@ int Invoice::getQuantidade(){
 	return quantidade;
 }
 
-int Invoice::getPrecop(){
+double Invoice::getPrecop(){
 	return precop;
 }
 
 std::string Invoice::getDescricao(){
-	return std::descricao;
+	return descricao;
 }
 
 
 
 
 Invoice::Invoice(int numero, std::string descricao, int quantidade, double precop){
-	
+
 	if(quantidade < 0){
 		quantidade = 0;
-		std::cout << "Quantidade = " << quantidade << std::endl;   	
+		std::cout << "Quantidade = " << quantidade << std::endl;
 	}
 
 	if(precop < 0){
 		precop = 0.0;
-		std::cout << "Preco do produto = " << precop << std::endl;	
-	}	
+		std::cout << "Preco do produto = " << precop << std::endl;
+	}
 
 }
 
 double Invoice::getInvoiceAmount(int quantidade, double precop){
-	
+
 	double precof;
-	
+
 	precof = quantidade * precop;
-	
+
 	std::cout << "Valor da fatura: " << precof << std::endl;
 
+return 0;
 }
+
