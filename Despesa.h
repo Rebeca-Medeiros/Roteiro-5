ca6b152e6f83fbfa9130996930fc5d8f986ba277
Rebeca Medeiros <rebeca.andrade.medeiros@gmail.com>
2017-09-14 01:34:50 -0300

    Update Despesa.h

--- Despesa.h
+++ Despesa.h
@@ -1,25 +1,19 @@
-#ifndef DESPESA_H
-#define DESPESA_H
-
-#include <iostream>
-#include <string>
-
-class Despesa{
-  private:
-    double gastos;
-    string tipoDeGasto;
-  public:
-    Despesa(double gastos, string tipoDeGasto);
-
-    double getGastos();
-    void setGastos(double g);
-
-    string getTipoDeGasto();
-    void setTipoDeGasto(string tp);
-
-};
-
-
-
-
-#endif // DESPESA_H
++#ifndef DESPESA_H
++#define DESPESA_H
++
++
++class Despesa
++{
++    public:
++        double getvalor();
++        void setvalor(double v);
++
++        double gettipoDeGasto();
++        void settipoDeGasto(double tdg);
++
++    private:
++        double valor;
++        double tipoDeGasto;
++};
++
++#endif // DESPESA_H
